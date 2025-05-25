bf709eccc159b2eacce9519ecb8809d3a7c91235
roxanamanea29 <manea.roxanaa@gmail.com>
2025-05-26 00:42:21 +0200

    Add the profile of the user into  the dashboard User.

---
+++ src/pages/user/PersonalView.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Personal() {
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center mb-4">Información Personal</h1>
+            <p>Esta sección está en desarrollo. Pronto podrás ver y editar tu información personal.</p>
+            <p>¡Gracias por tu paciencia!</p>
+        </div>
+    );
+}
