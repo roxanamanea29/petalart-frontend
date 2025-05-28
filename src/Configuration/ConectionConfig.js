29cfb0088ca782f761ed2734d346ac258f78c64b
roxanamanea29 <manea.roxanaa@gmail.com>
2025-05-28 22:20:22 +0200

    sin / to the  url in config .

--- src/Configuration/ConectionConfig.js
+++ src/Configuration/ConectionConfig.js
@@ -1,5 +1,5 @@
 /*const LOCALSERVERBASEURL =import.meta.env.VITE_API_URL || "http://localhost:8080";*/
-const LOCALSERVERBASEURL = "https://petalart-backend.onrender.com/";
+const LOCALSERVERBASEURL = "https://petalart-backend.onrender.com";
 
 
 export default LOCALSERVERBASEURL;
