c4d668f674df3c85dd846315d1f1198a7c68752b
roxanamanea29 <manea.roxanaa@gmail.com>
2025-05-30 14:04:06 +0200

    modify main to import emailjs form emails/browser

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 emailjs.init('CI45b8UMxUYMyNhFT')
 createRoot(document.getElementById('root')).render(
